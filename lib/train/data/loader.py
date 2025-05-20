2fe424dbd38ac9a87230c99e5ab8abda5c0bc227
hossammosh <37549426+hossammosh@users.noreply.github.com>
2025-05-20 07:14:09 +0300

    testing the laoder functionelity

--- lib/train/data/loader.py
+++ lib/train/data/loader.py
@@ -19,11 +19,9 @@ def _check_use_shared_memory():
 
 def ltr_collate(batch):
     """Puts each data field into a tensor with outer dimension batch size"""
-    print('batch = ', batch)
+    print('ltr_collate error batch = ')
     error_msg = "batch must contain tensors, numbers, dicts or lists; found {}"
     elem_type = type(batch[0])
-
-
 
     if isinstance(batch[0], torch.Tensor):
         out = None
@@ -73,7 +71,7 @@ def ltr_collate(batch):
 
 def ltr_collate_stack1(batch):
     """Puts each data field into a tensor. The tensors are stacked at dim=1 to form the batch"""
-
+    print('ltr_collate_stack1 batch = ')
     error_msg = "batch must contain tensors, numbers, dicts or lists; found {}"
     elem_type = type(batch[0])
     if isinstance(batch[0], torch.Tensor):
